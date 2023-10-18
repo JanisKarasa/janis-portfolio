82bd793613430c12b25c5c37d6d6add987f39e8c
Janis Karasa <jkarasa@gmail.com>
2023-10-18 10:52:06 +0200

    Updated project thumbnails, added CV icon and CV.PDF. Additionally, added Google Analitics script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Script that changes reCAPTCHA theme to dark by injecting data-theme attribute as dark
 document.addEventListener("DOMContentLoaded", (event) => {
   const recaptcha = document.querySelector(".g-recaptcha");
   recaptcha.setAttribute("data-theme", "dark");
